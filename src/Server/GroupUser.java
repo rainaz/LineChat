fe250ed3352d5533ae44af6fee7fa95d8d3f5386
YumeYami <iscream.p@gmail.com>
2014-02-08 17:51:11 +0700

    EditGrMessage

--- src/Server/GroupUser.java
+++ src/Server/GroupUser.java
@@ -1,5 +1,14 @@
 package Server;
 
-public class GroupUser {
+import java.util.ArrayList;
+import java.util.Vector;
 
+import Message.Message;
+
+public class GroupUser {
+	private String groupID;
+	private Vector<UserInfo> userInfos;
+	private ArrayList<Message> messages;
+	private int totalUser;
+	private int lastMessageNumber;
 }
